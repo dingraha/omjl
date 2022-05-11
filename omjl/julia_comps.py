9579c73ffa3533a56c9d3323d3c211ab07e6ae9b
Daniel Ingraham <d.j.ingraham@gmail.com>
2022-05-10 23:37:04 -0400

    Better tests and stuff

--- omjl/julia_comps.py
+++ omjl/julia_comps.py
@@ -1,6 +1,8 @@
 from types import MethodType
+import numpy as np
 
 import openmdao.api as om
+from openmdao.utils.class_util import overrides_method
 
 # The PythonCall/JuliaCall docs say I should create a new module to avoid polluting Main, but then the seval command below doesn't work.
 # import juliacall; jl = juliacall.newmodule("OMJL")
@@ -40,22 +42,37 @@ class JuliaExplicitComp(om.ExplicitComponent):
     def setup(self):
         _setup_common(self)
 
+        if jl.OpenMDAOCore.has_compute_partials(self._jlcomp):
+            def compute_partials(self, inputs, partials):
+                inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
+
+                partials_dict = {}
+                for of_wrt in self._declared_partials:
+                    partials_dict[of_wrt] = partials[of_wrt]
+                partials_dict = juliacall.convert(jl.Dict, partials_dict)
+
+                jl.OpenMDAOCore.compute_partials_b(self._jlcomp, inputs_dict, partials_dict)
+
+            # https://www.ianlewis.org/en/dynamically-adding-method-classes-or-class-instanc
+            self.compute_partials = MethodType(compute_partials, self)
+            # Hmm...
+            self._has_compute_partials = True
+
+        if jl.OpenMDAOCore.has_compute_jacvec_product(self._jlcomp):
+            def compute_jacvec_product(self, inputs, d_inputs, d_outputs, mode):
+                inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
+                d_inputs_dict = juliacall.convert(jl.Dict, dict(d_inputs))
+                d_outputs_dict = juliacall.convert(jl.Dict, dict(d_outputs))
+
+                jl.OpenMDAOCore.compute_jacvec_product_b(self._jlcomp, inputs_dict, d_inputs_dict, d_outputs_dict, mode)
+
+            self.compute_jacvec_product = MethodType(compute_jacvec_product, self)
+
     def compute(self, inputs, outputs):
         inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
         outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
 
         jl.OpenMDAOCore.compute_b(self._jlcomp, inputs_dict, outputs_dict)
-
-    def compute_partials(self, inputs, partials):
-        inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
-
-        partials_dict = {}
-        for of_wrt in self._declared_partials:
-            partials_dict[of_wrt] = partials[of_wrt]
-        partials_dict = juliacall.convert(jl.Dict, partials_dict)
-
-        jl.OpenMDAOCore.compute_partials_b(self._jlcomp, inputs_dict, partials_dict)
-
 
 class JuliaImplicitComp(om.ImplicitComponent):
 
@@ -89,39 +106,67 @@ class JuliaImplicitComp(om.ImplicitComponent):
         #    # https://www.ianlewis.org/en/dynamically-adding-method-classes-or-class-instanc
         #    self.apply_linear = MethodType(apply_linear, self)
 
-    def apply_nonlinear(self, inputs, outputs, residuals):
-        inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
-        outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
-        residuals_dict = juliacall.convert(jl.Dict, dict(residuals))
+        if jl.OpenMDAOCore.has_apply_nonlinear(self._jlcomp):
+            def apply_nonlinear(self, inputs, outputs, residuals):
+                inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
+                outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
+                residuals_dict = juliacall.convert(jl.Dict, dict(residuals))
 
-        jl.OpenMDAOCore.apply_nonlinear_b(self._jlcomp, inputs_dict, outputs_dict, residuals_dict)
+                jl.OpenMDAOCore.apply_nonlinear_b(self._jlcomp, inputs_dict, outputs_dict, residuals_dict)
 
-    def linearize(self, inputs, outputs, partials):
-        inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
-        outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
+            self.apply_nonlinear = MethodType(apply_nonlinear, self)
 
-        partials_dict = {}
-        for of_wrt in self._declared_partials:
-            partials_dict[of_wrt] = partials[of_wrt]
-        partials_dict = juliacall.convert(jl.Dict, partials_dict)
+        if jl.OpenMDAOCore.has_solve_nonlinear(self._jlcomp):
+            def solve_nonlinear(self, inputs, outputs):
+                inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
+                outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
 
-        jl.OpenMDAOCore.linearize_b(self._jlcomp, inputs_dict, outputs_dict, partials_dict)
+                jl.OpenMDAOCore.solve_nonlinear_b(self._jlcomp, inputs_dict, outputs_dict)
 
-    def guess_nonlinear(self, inputs, outputs, residuals):
-        pass
-        # if self._julia_guess_nonlinear:
-        #     inputs_dict = dict(inputs)
-        #     outputs_dict = dict(outputs)
-        #     residuals_dict = dict(residuals)
+            self.solve_nonlinear = MethodType(solve_nonlinear, self)
 
-        #     self._julia_guess_nonlinear(self._jlcomp, inputs_dict, outputs_dict,
-        #                                 residuals_dict)
+        if jl.OpenMDAOCore.has_linearize(self._jlcomp):
+            def linearize(self, inputs, outputs, partials):
+                inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
+                outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
 
-    def solve_nonlinear(self, inputs, outputs):
-        pass
-        # if self._julia_solve_nonlinear:
-        #     inputs_dict = dict(inputs)
-        #     outputs_dict = dict(outputs)
+                partials_dict = {}
+                for of_wrt in self._declared_partials:
+                    partials_dict[of_wrt] = partials[of_wrt]
+                partials_dict = juliacall.convert(jl.Dict, partials_dict)
 
-        #     self._julia_solve_nonlinear(self._jlcomp, inputs_dict, outputs_dict)
+                jl.OpenMDAOCore.linearize_b(self._jlcomp, inputs_dict, outputs_dict, partials_dict)
 
+            self.linearize = MethodType(linearize, self)
+
+        if jl.OpenMDAOCore.has_apply_linear(self._jlcomp):
+            def apply_linear(self, inputs, outputs, d_inputs, d_outputs, d_residuals, mode):
+                inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
+                outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
+                d_inputs_dict = juliacall.convert(jl.Dict, dict(d_inputs))
+                d_outputs_dict = juliacall.convert(jl.Dict, dict(d_outputs))
+                d_residuals_dict = juliacall.convert(jl.Dict, dict(d_residuals))
+
+                jl.OpenMDAOCore.apply_linear_b(self._jlcomp, inputs_dict, outputs_dict,
+                        d_inputs_dict, d_outputs_dict, d_residuals_dict, mode)
+
+            self.apply_linear = MethodType(apply_linear, self)
+
+        if jl.OpenMDAOCore.has_solve_linear(self._jlcomp):
+            def solve_linear(self, d_outputs, d_residuals, mode):
+                d_outputs_dict = juliacall.convert(jl.Dict, dict(d_outputs))
+                d_residuals_dict = juliacall.convert(jl.Dict, dict(d_residuals))
+
+                jl.OpenMDAOCore.solve_linear_b(self._jlcomp, d_outputs_dict, d_residuals_dict, mode)
+
+        if jl.OpenMDAOCore.has_guess_nonlinear(self._jlcomp):
+            def guess_nonlinear(self, inputs, outputs, residuals):
+                inputs_dict = juliacall.convert(jl.Dict, dict(inputs))
+                outputs_dict = juliacall.convert(jl.Dict, dict(outputs))
+                residuals_dict = juliacall.convert(jl.Dict, dict(residuals))
+
+                jl.OpenMDAOCore.guess_nonlinear_b(self._jlcomp, inputs_dict, outputs_dict, residuals_dict)
+
+            self.guess_nonlinear = MethodType(guess_nonlinear, self)
+            # Hmm...
+            self._has_guess = True
--- tests/test_ecomp.py
+++ tests/test_ecomp.py
@@ -1,3 +1,5 @@
+import time
+
 from pytest import approx
 
 import openmdao.api as om
@@ -28,11 +30,12 @@ end
 
 function OpenMDAOCore.compute_partials!(self::EComp1, inputs, partials)
     partials["y", "x"][1] = 4*inputs["x"][1]
+    @show 4*inputs["x"]
     return nothing
 end
 
 struct EComp2 <: OpenMDAOCore.AbstractExplicitComp
-  a::Float64
+    a::Float64
 end
 
 function OpenMDAOCore.setup(self::EComp2)
@@ -53,6 +56,30 @@ function OpenMDAOCore.compute_partials!(self::EComp2, inputs, partials)
     return nothing
 end
 
+struct EComp3 <: OpenMDAOCore.AbstractExplicitComp
+    a::Vector{Float64}
+end
+
+EComp3(n::Integer) = EComp3(range(1.0, n; length=n) |> collect)
+
+function OpenMDAOCore.setup(self::EComp3)
+    input_data = [VarData("x")]
+    output_data = [VarData("y")]
+    partials_data = [PartialsData("y", "x")]
+
+    return input_data, output_data, partials_data
+end
+
+function OpenMDAOCore.compute!(self::EComp3, inputs, outputs)
+    outputs["y"][1] = 2*self.a[1]*inputs["x"][1]^2 + 1
+    return nothing
+end
+
+function OpenMDAOCore.compute_partials!(self::EComp3, inputs, partials)
+    partials["y", "x"][1] = 4*self.a[1]*inputs["x"][1]
+    return nothing
+end
+
 end # module
 """)
 
@@ -63,6 +90,7 @@ def test1():
     comp = julia_comps.JuliaExplicitComp(jlcomp=ecomp)
     p.model.add_subsystem("ecomp", comp, promotes_inputs=["x"], promotes_outputs=["y"])
     p.setup()
+    p.final_setup()
     p.set_val("x", 3.0)
     p.run_model()
     print(p.compute_totals(of="y", wrt="x"))
@@ -81,3 +109,38 @@ def test2():
     p.run_model()
     assert p.get_val("y")[0] == approx(2*a*p.get_val("x")[0]**2 + 1)
     assert p.compute_totals(of="y", wrt="x")["y", "x"][0,0] == approx(4*a*p.get_val("x")[0])
+
+def test3():
+    times_d = {}
+    for n in [10, 10000000]:
+        # Run once to get rid of the Julia JIT time.
+        p = om.Problem()
+        ecomp = jl.EComp1Test.EComp3(n)
+        comp = julia_comps.JuliaExplicitComp(jlcomp=ecomp)
+        p.model.add_subsystem("ecomp", comp, promotes_inputs=["x"], promotes_outputs=["y"])
+        p.setup()
+        p.set_val("x", 3.0)
+        p.run_model()
+        assert p.get_val("y")[0] == approx(2*p.get_val("x")[0]**2 + 1)
+        assert p.compute_totals(of="y", wrt="x")["y", "x"][0,0] == approx(4*p.get_val("x")[0])
+
+        print(f"n = {n}")
+        times = []
+        samples = 50
+        for _ in range(samples):
+            p = om.Problem()
+            ecomp = jl.EComp1Test.EComp3(n)
+            comp = julia_comps.JuliaExplicitComp(jlcomp=ecomp)
+            p.model.add_subsystem("ecomp", comp, promotes_inputs=["x"], promotes_outputs=["y"])
+            p.setup()
+            p.set_val("x", 3.0)
+            t = time.time()
+            p.run_model()
+            assert p.get_val("y")[0] == approx(2*p.get_val("x")[0]**2 + 1)
+            assert p.compute_totals(of="y", wrt="x")["y", "x"][0,0] == approx(4*p.get_val("x")[0])
+            times.append(time.time() - t)
+            print(f"time = {time.time() - t}")
+
+        times_d[n] = sum(times)/samples
+
+    assert times_d[10] == approx(times_d[10000000], rel=1e-1)
